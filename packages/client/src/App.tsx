dd5a8c51d0c8833c4cd52ff46ede04fc9cf753a9
Vijay Kasi Viswanath <vkviswanath0044@gmail.com>
2025-10-20 20:12:05 +0530

    set up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    return (
       <div className="p-4">
          <p className="font-bold p-4 text-3xl">{message}</p>
-         <Button>click me</Button>
+         <Button>Click me</Button>
       </div>
    );
 }
